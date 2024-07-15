d9a82f2e58907eed948d1847b9f0e72a1ca1fd65
Nicholas <NLoo1@outlook.com>
2024-07-15 17:34:10 -0400

    Add insert, find

--- binary-search-tree.js
+++ binary-search-tree.js
@@ -14,59 +14,85 @@ class BinarySearchTree {
   /** insert(val): insert a new node into the BST with value val.
    * Returns the tree. Uses iteration. */
 
-  insert(val){
-    // if(this.root == null) {
-    //   this.root = new Node(val)
-    //   return this
-    // } else{
-    const cursor = this.root 
-    while(cursor.left !==null || cursor.right !== null){
-      switch(val){
-        case val > cursor.val:
-          cursor = cursor.right
-          break;
-        case val < cursor.val:
-          cursor = cursor.left
-          break;
-        case val == cursor.val:
-          return "Val already in tree"
-      }
-    }
-    
-    if(val > cursor.val) cursor.right = new Node(val)
-    else if(val < cursor.val) cursor.left = new Node(val)
+  insert(val) {
+    let cursor = this.root;
 
-    return this
-// }
-  }
+    if (cursor === null) {
+        this.root = new Node(val);
+        return this;
+    } else {
+
+      // Traverse through tree, iteratively
+        while (true) {
+
+          // Go right if val is greater than cursor
+            if (val > cursor.val) {
+              // If you can't go right, make new Node there
+                if (cursor.right === null) {
+                    cursor.right = new Node(val);
+                    break;
+                }
+                cursor = cursor.right;
+            }
+            // Go left if val is less than cursor 
+            else if (val < cursor.val) {
+              // Set as new node if there is no left node
+                if (cursor.left === null) {
+                    cursor.left = new Node(val);
+                    break;
+                }
+
+                // Otherwise, traverse left
+                cursor = cursor.left;
+            } else {
+                return "Val already in tree";
+            }
+        }
+    }
+
+    // Return tree
+    return this;
+}
+
 
   /** insertRecursively(val): insert a new node into the BST with value val.
    * Returns the tree. Uses recursion. */
 
   insertRecursively(val) {
-    if(cursor == undefined){
-      const cursor = this.root
-    }
-    // Edge case: val already in tree
-    if(cursor.val == val) return "Val already exists in tree"
 
-    const cursor = this.root 
-    if(cursor.left == null || cursor.right == null){
-      if(val > cursor.val) cursor.right = new Node(val)
-      else cursor.left = new Node(val) 
-
+    // Edge case: if no root is there, set to root
+    if(!this.root){
+      this.root = new Node(val)
       return this
     }
 
-    if(val > cursor.val) this.insertRecursively(this.right)
-    if(val < cursor.val) this.insertRecursively(this.left)
+    // If val > root
+    if(val > this.root){
+
+      // BUT if right Node does not exist, insert Node and return
+      if(this.right == null){
+        this.right = new Node(val)
+        return this
+      }
+      // Otherwise, go right and run again
+      // ******* HOW TO CALL AGAIN???
+      return this.insertRecursively(val)
+    } else if(val < this.root){
+      if(this.left == null){
+        this.left = new Node(val)
+        return this
+      }
+      return this.insertRecursively(this.left)
+    }
+
+    return this
   }
 
   /** find(val): search the tree for a node with value val.
    * return the node, if found; else undefined. Uses iteration. */
 
   find(val) {
-    const cursor = this.root
+    let cursor = this.root
     while(cursor){
       if(val == cursor.val) return cursor
       else if(val > cursor.val) cursor = cursor.right
